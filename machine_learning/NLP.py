6f0a6c68fe9851213810222706cea171302acb27
Javi Carrascosa <70140940+jacarbas@users.noreply.github.com>
2023-06-09 10:06:20 +0200

    Add files via upload

---
+++ machine_learning/NLP.py
@@ -0,0 +1,12 @@
+from spanlp.palabrota import Palabrota
+from better_profanity import profanity
+
+palabrota = Palabrota()
+
+text = "Tus muertos cabron"
+#returns True if it contains bad language
+print(palabrota.contains_palabrota(text))
+
+dirty_text = "That l3sbi4n did a very good H4ndjob."
+#returns True if it contains bad language
+profanity.contains_profanity(dirty_text)
